f6d2d71dca7d68af0c1939385c40d824ec262a71
CJerrong <66013323+CJerrong@users.noreply.github.com>
2025-02-19 18:13:43 +0800

    Add api call for bus stops under views

--- backend/bus_coming/views.py
+++ backend/bus_coming/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from decouple import config
+from decouple import Config, RepositoryEnv, AutoConfig
 import requests
+
+
+
+class BusStopsView(APIView):
+    def get(self, request):
+        config = Config(RepositoryEnv("/bus_coming_proj/backend/bus_coming_site/.env"))
+        url = "https://datamall2.mytransport.sg/ltaodataservice/BusStops"
+        API_KEY = config("API_KEY")
+        headers = {
+            'AccountKey': API_KEY,
+            'accept': 'application/json'
+        }
+        
+        # Need to iterate through as datamall restricts requests to 500 lines
+        # Need to loop through until no more bus stops
+        all_bus_stops = []
+        skip = 0
+        
+        while True:
+            response = requests.get(url, headers=headers, params={'$skip':skip})
+            
+            if response.status_code != 200:
+                return Response(
+                    {'error: Failed to fetch bus stops'},
+                    status=response.status_code
+                )
+            
+            data = response.json()
+            bus_stops = data.get('value', [])
+            
+            # No more bus stops
+            if not bus_stops:
+                break
+            
+            all_bus_stops.extend(bus_stops)
+            skip += 500
+        
+        return Response(all_bus_stops)
+            
+            
 
 class BusTimingView(APIView):
     def get(self, request, bus_stop_id):
@@ -11,7 +51,7 @@ class BusTimingView(APIView):
         url = config("BUS_TIMING_URL")
 
         headers = {
-            'AccountKey': API_KEY
+            'AccountKey': API_KEY,
             'accept': 'application/json'
         }
 
